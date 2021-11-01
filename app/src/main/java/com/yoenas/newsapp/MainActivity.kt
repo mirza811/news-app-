b8562a014e11cba6582ded0dd105c5493c8a47ce
yoenas <yusrilnurhadi19@gmail.com>
2021-11-01 08:17:52 +0700

    Setting TabLayoutMediator

--- app/src/main/java/com/yoenas/newsapp/MainActivity.kt
+++ app/src/main/java/com/yoenas/newsapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.yoenas.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        val vpNews: ViewPager2 = findViewById(R.id.vp_news)
+        val mSectionPagerAdapter = SectionPagerAdapter(this)
+        vpNews.adapter = mSectionPagerAdapter
+
+        val tabs: TabLayout = findViewById(R.id.tabs)
+
+        val tabList = arrayOf("All News", "Top", "Popular")
+        TabLayoutMediator(tabs, vpNews) { tab, position ->
+            tab.text = tabList[position]
+        }.attach()
     }
 }
---
+++ app/src/main/java/com/yoenas/newsapp/PopularFragment.kt
@@ -0,0 +1,18 @@
+package com.yoenas.newsapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PopularFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_popular, container, false)
+    }
+}
