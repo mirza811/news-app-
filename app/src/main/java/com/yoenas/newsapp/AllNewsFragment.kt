99242dd47d19c1c80c291b19e2319cc41e1d80dc
yoenas <yusrilnurhadi19@gmail.com>
2021-11-01 13:43:42 +0700

    Show News on RecyclerView

--- app/src/main/java/com/yoenas/newsapp/AllNewsFragment.kt
+++ app/src/main/java/com/yoenas/newsapp/AllNewsFragment.kt
@@ -1,19 +1,27 @@
 package com.yoenas.newsapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class AllNewsFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_all_news, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_all_news, container, false)
 
+        val rvNews = view.findViewById<RecyclerView>(R.id.rv_all_news)
+        rvNews.layoutManager = LinearLayoutManager(view.context)
+        rvNews.adapter = NewsAdapter(DataNews.listAllNews)
+
+        return view
+    }
 }
---
+++ app/src/main/java/com/yoenas/newsapp/News.kt
@@ -0,0 +1,11 @@
+package com.yoenas.newsapp
+
+data class News(
+    val title: String,
+    val content: String,
+    val date: String,
+    val author: String,
+    val time: String,
+    val category: String,
+    val image: Int
+)
